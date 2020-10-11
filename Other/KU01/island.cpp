0392e7608bf519915e07628324123fbcb9d222e8
PanaWanit <pnaw.wan@gmail.com>
2020-10-11 17:24:10 +0700

    Other

--- Other/KU01/island.cpp
+++ Other/KU01/island.cpp
@@ -9,10 +9,8 @@ using namespace std;
 vector<int> ls;
 int main(){
 	ios::sync_with_stdio(0),cin.tie(0);
-	/*
-	******************* CASE K > 1 *********************************
 	int n,m,sum=0,cnt=0; // cnt = island
-	bool ch=0;
+	bool ch=0; // ch = 1 -> last = minus
 	cin >> n >> m;
 	for(int i=1,x; i<=n; i++){
 		cin >> x;
@@ -22,9 +20,7 @@ int main(){
 	ls.push_back(sum);
 	int ans=0;
 	sort(ls.begin(),ls.end());
-	for(int i=0; i<cnt-m; i++) ans += ls[i];
+	for(int i=0; i<cnt-m; i++) ans += ls[i]; // plus 1 time => decrease 1 island
 	cout << ans << "\n";
-	*****************************************************************
-	*/
 	return 0;
 }
