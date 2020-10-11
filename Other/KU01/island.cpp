86ace59205a99ef4e5a9bac37e5c705f38ea0d74
PanaWanit <pnaw.wan@gmail.com>
2020-10-11 16:56:27 +0700

    WRONG

--- Other/KU01/island.cpp
+++ Other/KU01/island.cpp
@@ -9,6 +9,8 @@ using namespace std;
 vector<int> ls;
 int main(){
 	ios::sync_with_stdio(0),cin.tie(0);
+	/*
+	******************* CASE K > 1 *********************************
 	int n,m,sum=0,cnt=0; // cnt = island
 	bool ch=0;
 	cin >> n >> m;
@@ -17,10 +19,12 @@ int main(){
 		if(x > 0 && ch) ls.push_back(sum),sum=0,ch=0,cnt++;
 		if(x < 0) ch=1,sum -= x-1;
 	}
-	if(sum != 0) ls.push_back(sum);
+	ls.push_back(sum);
 	int ans=0;
 	sort(ls.begin(),ls.end());
 	for(int i=0; i<cnt-m; i++) ans += ls[i];
 	cout << ans << "\n";
+	*****************************************************************
+	*/
 	return 0;
 }
