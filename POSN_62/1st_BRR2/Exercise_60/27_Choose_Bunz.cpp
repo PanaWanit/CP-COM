72cbbe23babb02daa564b5ef2479b553745d19ae
PanaWanit <pnaw.wan@gmail.com>
2020-11-28 10:26:39 +0700

    C

--- POSN_62/1st_BRR2/Exercise_60/27_Choose_Bunz.cpp
+++ POSN_62/1st_BRR2/Exercise_60/27_Choose_Bunz.cpp
@@ -6,25 +6,17 @@
  */
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<int,pii> pip;
-#define fp first
-#define sp second
-#define pb push_back
-#define eb emplace_back
-#define all(x) (x).begin(),(x).end()
 const int mxN = 1e4+1;
 int dp[2][mxN];
 int main(){
 	ios::sync_with_stdio(0),cin.tie(0);
 	int n,r,mod;
 	cin >> n >> r >> mod;
-	dp[1][0] = 1%mod;
-	for(int i=1; i<=n; i++){
+	dp[1][0] = dp[1][1] = 1%mod;
+	for(int i=2; i<=n; i++) {
 		dp[i%2][0] = 1;
-		for(int j=1; j<=r; j++){
-			dp[i%2][j] = (dp[(i+1)%2][j-1] + dp[(i+1)%2][j])%mod; // C(i,j) = C(i-1,j) + C(i-1,j-1) Pascal Triangle LOL
+		for(int j=1; j<=min(i,r); j++) {
+			dp[i%2][j] = (dp[(i+1)%2][j-1] + dp[(i+1)%2][j])%mod;
 		}
 	}
 	cout << dp[n%2][r] << "\n";
