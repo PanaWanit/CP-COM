0aefd228ee0b5fedfbe709f959f86daac708c66b
PanaWanit <pnaw.wan@gmail.com>
2020-12-25 20:25:51 +0700

    template-update

--- template.cpp
+++ template.cpp
@@ -1,27 +1,10 @@
-/*
-	Task: 
-	Author: Pana Wanit
-	Lang: C++
-	School: RYW
-*/
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<int,pii> pip;
+using ll = long long;
 #define vt vector
-#define pb push_back
-#define eb emplace_back
-#define sz(x) (int)(x).size()
-#define all(x) (x).begin(),(x).end()
-#define _lb lower_bound
-#define _ub upper_bound
-#define foe(x,a) for(auto& x : a)
-void DBG() {cerr << "]" << endl;}
-template<class A,class... T> void DBG(A x,T... t) {cerr << x; if(sizeof...(t)) cerr << ","; DBG(t...);}
-#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:[",DBG(__VA_ARGS__)
+#define ar array
 int main() {
-	ios::sync_with_stdio(0),cin.tie(0);
+	ios::sync_with_stdio(false), cin.tie(nullptr);
 
 	return 0;
 }
