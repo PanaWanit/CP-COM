0fb8e1adcb4869442fe440cb2940cb7e6d91ec64
PanaWanit <pnaw.wan@gmail.com>
2021-04-04 22:20:41 +0700

    04-04-2021v3

--- Pre_OSN17/RT_Econ_way.cpp
+++ Pre_OSN17/RT_Econ_way.cpp
@@ -1,8 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int mxN = 202;
+const int mxN = 205;
+int a[mxN][mxN],dp[mxN][mxN];
 int main() {
-
+	int n;
+	scanf("%d", &n);
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+a[i][j]});
+		}
+	}
+	printf("%d\n", dp[n][n]);
 	return 0;
 }
 
