66b2b49b197399fa14693010ef414da84f79864c
PanaWanit <pnaw.wan@gmail.com>
2021-04-29 11:28:06 +0700

    template.cpp update

--- template.cpp
+++ template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define ll long long
 #define ar array
 #define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
 
 int main() {
 	ios::sync_with_stdio(false), cin.tie(nullptr);
